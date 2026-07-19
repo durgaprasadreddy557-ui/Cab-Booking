ad09c87c2ecdcc2420601cfb6066af27ec7db8eb
durgaprasadreddy557-ui <durgaprasadreddy557@gmail.com>
2026-07-19 14:35:32 +0530

    Remove license header from Header component
    Removed license comment from Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,3 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import React, { useState } from 'react';
 import { Menu, LogOut, ShieldAlert, Award, User, HelpCircle, UserCheck } from 'lucide-react';
 import { HOTLINKED_IMAGES } from '../data';
